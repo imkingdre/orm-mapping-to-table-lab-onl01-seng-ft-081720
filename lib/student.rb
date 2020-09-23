d54afd1d559db549707b4988eb75004743e28520
Andre King <andreking1031@gmail.com>
2020-09-23 15:42:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,43 +2,47 @@ require_relative "../config/environment.rb"
 
 
 class Student
+
   attr_accessor :name, :grade
   attr_reader :id
-  
-  def initialize(name, grade, id=nil)
-    @id = id
+
+  def initialize (name, grade, id = nil)
     @name = name
     @grade = grade
+    @id = id
   end
-  
+
   def self.create_table
-    sql =  <<-SQL 
+    sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-      id INTERGER PRIMARY KEY,
-      name TEXT
-      grade INTERGER
-      )
-      SQL
-      
-      DB[:conn].execute(sql) 
+        id INTEGER PRIMARY KEY, 
+        name TEXT,
+        grade INTEGER
+        )
+    SQL
+      DB[:conn].execute(sql)
   end
-  
+
   def self.drop_table
     sql = <<-SQL
-    DROP TABLE students
+      DROP TABLE students
     SQL
-    
-    DB[:conn].execute(sql)
-    
+
+    DB[:conn].execute(sql)  
   end
- 
- 
- def save
+
+  def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
         VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
  
