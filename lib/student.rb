0be6cff9a54d4e7770ee2e6e50857ce16e700cdc
Andre King <andreking1031@gmail.com>
2020-09-23 15:25:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
+  def initialize(name, )
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
